bb05d5c91a43a5bb49a40196218121307a89487f
Gilbert <s160419105@student.ubaya.ac.id>
2022-03-09 01:23:38 +0700

    tutorial week 4 selesai

---
+++ app/src/main/java/com/ubaya/adv160419105week4/model/Model.kt
@@ -0,0 +1,9 @@
+package com.ubaya.adv160419105week4.model
+
+data class Student(
+    val id:String?,
+    var name:String?,
+    var dob:String?,
+    var phone:String?,
+    var photoUrl:String?
+)
