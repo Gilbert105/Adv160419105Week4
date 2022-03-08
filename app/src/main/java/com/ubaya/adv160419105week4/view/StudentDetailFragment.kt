f293f7e4af4b0ddc11c433d52888c42117b964f3
Gilbert <s160419105@student.ubaya.ac.id>
2022-03-09 01:45:33 +0700

    Homework week 4 selesai

--- app/src/main/java/com/ubaya/adv160419105week4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/ubaya/adv160419105week4/view/StudentDetailFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.ubaya.adv160419105week4.R
+import com.ubaya.adv160419105week4.viewmodel.DetailViewModel
+import com.ubaya.adv160419105week4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_detail.*
 
 /**
  * A simple [Fragment] subclass.
@@ -13,11 +17,27 @@ import com.ubaya.adv160419105week4.R
  * create an instance of this fragment.
  */
 class StudentDetailFragment : Fragment() {
+    private lateinit var detailModel : DetailViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_student_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        detailModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        detailModel.fetch()
+        observeDetailModel()
+    }
+
+    private fun observeDetailModel() {
+        detailModel.studentLiveData.observe(viewLifecycleOwner){
+            editID.setText(it.id)
+            editName.setText(it.name)
+            editBOD.setText(it.dob)
+            editPhone.setText(it.phone)
+        }
     }
 }
